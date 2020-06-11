4404bfb88df327321dd0093400c1e4d8b9f5a580
Arun Mohan Sharma <arunm2470@gmail.com>
2020-06-11 22:38:38 +0530

    Day 11

---
+++ Day11.cpp
@@ -0,0 +1,25 @@
+
+//pronlem link :- https://leetcode.com/explore/featured/card/june-leetcoding-challenge/540/week-2-june-8th-june-14th/3357/
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n=nums.size();
+        int low=0,mid=0,high=n-1;
+        
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[low],nums[mid]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                swap(nums[mid],nums[high]);
+                high--;
+            }
+        }
+        
+    }
+};
